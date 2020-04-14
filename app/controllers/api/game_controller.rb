fa94a249fedd7e48ddf9a3eebb4a76e959bf7871
jeroig <jeroig@gmail.com>
2020-04-14 09:41:12 -0300

    Clean code

--- app/controllers/api/game_controller.rb
+++ app/controllers/api/game_controller.rb
@@ -3,26 +3,21 @@ class Api::GameController < ApplicationController
 
   def to_click
     render json: @board.to_click(@row, @col)
-    #render json: Board2.instance.to_click(params[:row].to_i, params[:col].to_i)
   end
 
   def click
     render json: @board.click(@row, @col)
-    #render json: Board2.instance.click(params[:row].to_i, params[:col].to_i)
   end
 
   def mark
     render json: @board.mark(@row, @col)
-    #render json: Board2.instance.mark(params[:row].to_i, params[:col].to_i)
   end
 
   def question
     render json: @board.question(@row, @col)
-    #render json: Board2.instance.question(params[:row].to_i, params[:col].to_i)
   end
 
   def reset
-    #render json: Board2.instance.reset(params[:rows].to_i, params[:columns].to_i, params[:mines].to_i)
     @board = Board.new(user_id: @user.id, rows: params[:rows].to_i, columns: params[:columns].to_i, mines: params[:mines].to_i, timer: 0, state: 'playing')
     if @board.save
       render json: {
